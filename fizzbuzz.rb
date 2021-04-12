1e9c327b454cdd3d9c550457307ff60aca2cdf19
Eric Ontiveros <ericontiveros123@gmail.com>
2021-04-12 23:29:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,8 @@
 #require 'pry'
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0 
+    "Fizz" 
   elsif
      int % 5 == 0
     "Buzz"
