a28bb5be6338da12e7077880a2da9fb5ecbe0124
Eric Ontiveros <ericontiveros123@gmail.com>
2021-04-12 23:30:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,13 @@
 #require 'pry'
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
+  if int % 3 == 0 && int % 5 == 0
+  "FizzBuzz"
   elsif
      int % 5 == 0
     "Buzz"
   elsif
-    int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
+    int % 3 == 0
+    "Fizz"
  end
 end
